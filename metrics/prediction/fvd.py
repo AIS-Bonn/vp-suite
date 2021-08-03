Fix inf gradient from sqrt(0) in FVD; lots of changes regarding dataset quality

metrics/prediction/fvd.py
@@ -18,8 +18,9 @@ class FrechetVideoDistance(nn.Module):
     def __init__(self, num_frames, in_channels=3):
         super(FrechetVideoDistance, self).__init__()
 
-        if num_frames < 9:
-            raise ValueError(f"The I3D Module used for FVD needs at least 9 input frames (given: {num_frames})!")
+        if num_frames < 9 or num_frames > 16:
+            raise ValueError(f"The I3D Module used for FVD needs in between"
+                             f" 9 and 16 input frames (given: {num_frames})!")
 
         self.i3d_in_shape = (224, 224)
 
@@ -43,7 +44,7 @@ class FrechetVideoDistance(nn.Module):
         real = TF.resize(real.reshape(-1, *vid_shape[2:]), self.i3d_in_shape)
 
         # re-arrange dims for I3D input
-        pred = pred.reshape(*vid_shape[:3], *self.i3d_in_shape).permute((0, 2, 1, 3, 4))  # [b, T, c, 224, 224]
+        pred = pred.reshape(*vid_shape[:3], *self.i3d_in_shape).permute((0, 2, 1, 3, 4))  # [b, c, T, 224, 224]
         real = real.reshape(*vid_shape[:3], *self.i3d_in_shape).permute((0, 2, 1, 3, 4))
 
         logits_pred = self.i3d.extract_features(pred).squeeze()  # [b, n]

train_pred_model.py
@@ -38,11 +38,12 @@ def main(args):
     val_dir = os.path.join(data_dir, 'val')
     test_dir = os.path.join(data_dir, 'test')
     train_data = SynpickVideoDataset(data_dir=train_dir, num_frames=VIDEO_TOT_LENGTH,
-                                     step=4, allow_overlap=VID_DATA_ALLOW_OVERLAP)
+                                     step=VID_STEP, allow_overlap=VID_DATA_ALLOW_OVERLAP)
     val_data = SynpickVideoDataset(data_dir=val_dir, num_frames=VIDEO_TOT_LENGTH,
-                                   step=4, allow_overlap=VID_DATA_ALLOW_OVERLAP)
-    train_loader = DataLoader(train_data, batch_size=VID_BATCH_SIZE, shuffle=True, num_workers=VID_BATCH_SIZE)
-    valid_loader = DataLoader(val_data, batch_size=1, shuffle=False, num_workers=4)
+                                   step=VID_STEP, allow_overlap=VID_DATA_ALLOW_OVERLAP)
+    train_loader = DataLoader(train_data, batch_size=VID_BATCH_SIZE, shuffle=True, num_workers=VID_BATCH_SIZE,
+                              drop_last=True)
+    valid_loader = DataLoader(val_data, batch_size=1, shuffle=False, num_workers=4, drop_last=True)
 
     # MODEL
 
@@ -65,7 +66,7 @@ def main(args):
     }
     # FVD loss only available for 2- or 3- channel input
     if num_channels == 2 or num_channels == 3:
-        losses["fvd"] = (FrechetVideoDistance(num_frames=VIDEO_TOT_LENGTH, in_channels=num_channels), True, 0.0)
+        losses["fvd"] = (FrechetVideoDistance(num_frames=VIDEO_TOT_LENGTH, in_channels=num_channels), True, 0.001)
     # Check if indicator loss available
     if cfg.indicator_val_loss not in losses.keys():
         default_loss = "mse"
@@ -76,6 +77,8 @@ def main(args):
     optimizer = None
     if not cfg.no_train:
         optimizer = torch.optim.Adam(params=pred_model.parameters(), lr=LEARNING_RATE)
+
+    torch.autograd.set_detect_anomaly(True)
 
     # TRAINING
     for i in range(0, NUM_EPOCHS):
@@ -109,14 +112,17 @@ def main(args):
                 # bwd
                 optimizer.zero_grad()
                 loss.backward()
+                torch.nn.utils.clip_grad_norm_(pred_model.parameters(), 10)
                 optimizer.step()
 
-                loop.set_postfix(loss=loss.item())
+                # loop.set_postfix(loss=loss.item())
+                loop.set_postfix(mem=torch.cuda.memory_allocated())
         else:
             print("Skipping trianing loop.")
 
         print("Validating...")
-        val_losses = validate_vid_model(valid_loader, pred_model, DEVICE, VIDEO_IN_LENGTH, VIDEO_PRED_LENGTH, losses)
+        val_losses = validate_vid_model(valid_loader, pred_model, DEVICE, VIDEO_IN_LENGTH, VIDEO_PRED_LENGTH, losses,
+                                        data_in_type, num_channels)
         cur_val_loss = val_losses[cfg.indicator_val_loss]
 
         # save model if last epoch improved acc.
@@ -137,8 +143,9 @@ def main(args):
     print("\nTraining done, testing best model...")
     best_model = torch.load(str((out_dir / 'best_model.pth').resolve()))
     test_data = SynpickVideoDataset(data_dir=test_dir, num_frames=VIDEO_TOT_LENGTH, step=4)
-    test_loader = DataLoader(test_data, batch_size=1, shuffle=False, num_workers=4)
-    validate_vid_model(test_loader, best_model, DEVICE, VIDEO_IN_LENGTH, VIDEO_PRED_LENGTH, losses)
+    test_loader = DataLoader(test_data, batch_size=1, shuffle=False, num_workers=4, drop_last=True)
+    validate_vid_model(test_loader, best_model, DEVICE, VIDEO_IN_LENGTH, VIDEO_PRED_LENGTH, losses,
+                                        data_in_type, num_channels)
 
     print("Saving visualizations...")
     visualize_vid(test_data, VIDEO_IN_LENGTH, VIDEO_PRED_LENGTH, best_model, out_dir, vid_type, num_vis=10)

utils.py
@@ -48,7 +48,7 @@ def get_2_wasserstein_dist(pred, real):
     M_l = torch.matmul(C_pred.t(), C_real)
     M_r = torch.matmul(C_real.t(), C_pred)
     M = torch.matmul(M_l, M_r)
-    S = linalg.eigvals(M)
+    S = linalg.eigvals(M) + 1e-15  # add small constant to avoid infinite gradients from sqrt(0)
     sq_tr_cov = S.sqrt().abs().sum()
 
     # plug the sqrt_trace_component into Tr(cov_real + cov_pred - 2(cov_real * cov_pred)^(1/2))
@@ -188,13 +188,21 @@ def validate_seg_model(loader, seg_model, device):
     return 100.0 * num_correct / num_pixels
 
 
-def validate_vid_model(loader, pred_model, device, video_in_length, video_pred_length, losses):
+def validate_vid_model(loader, pred_model, device, video_in_length, video_pred_length, losses,
+                       data_in_type, num_channels):
 
     pred_model.eval()
     with torch.no_grad():
         loop = tqdm(loader)
         all_losses = {key: [] for key in losses.keys()}
-        for batch_idx, data in enumerate(loop):
+        for batch_idx, (imgs, masks, colorized_masks) in enumerate(loop):
+
+            if data_in_type == "rgb":
+                data = imgs
+            elif num_channels == 3:
+                data = colorized_masks
+            else:
+                data == masks
 
             # fwd
             data = data.to(device)  # [b, T, h, w], with T = video_tot_length