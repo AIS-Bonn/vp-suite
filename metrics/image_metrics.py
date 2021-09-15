8c3311e7ff430cc81818f49f3c7f45fe2845c8fb
Andreas Boltres <boltres@ais.uni-bonn.de>
2021-09-15 07:59:35 +0200

    added lpips metric

--- metrics/image_metrics.py
+++ metrics/image_metrics.py
@@ -1,8 +1,12 @@
 import numpy as np
 import torch
+
+import lpips
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import mean_squared_error as mse
+
+lpips_alex = lpips.LPIPS(net='alex') # LPIPS ver. 0.1.4
 
 def SSIM(pred, target):
     # expected value range: [-1.0, 1.0]
@@ -22,6 +26,9 @@ def MSE(pred, target):
     target = ((target + 1) * 255 / 2).astype(np.uint8)
     return mse(target, pred)
 
+def LPIPS(pred, target):
+    return lpips_alex(target, pred)
+
 
 def get_image_metrics(pred, target):
 
@@ -29,21 +36,22 @@ def get_image_metrics(pred, target):
         raise ValueError("Output images and target images are of different shape!")
 
     # create zipper list of pred-target numpy array pairs, removing other dimensions
-    pred = pred.view(-1, *pred.shape[2:])
-    pred = list(pred.detach().cpu().numpy())
-    target = target.view(-1, *target.shape[2:])
-    target = list(target.detach().cpu().numpy())
+    pred = pred.view(-1, *pred.shape[2:]).detach().cpu()
+    pred_torch, pred_numpy = list(pred), list(pred.numpy())
+    target = target.view(-1, *target.shape[2:]).detach().cpu()
+    target_torch, target_numpy = list(target), list(target.numpy())
 
     return {
-        "ssim": np.mean(np.stack([SSIM(p, t) for p, t in zip(pred, target)]), axis=0),
-        "psnr": np.mean(np.stack([PSNR(p, t) for p, t in zip(pred, target)]), axis=0),
-        "mse": np.mean(np.stack([MSE(p, t) for p, t in zip(pred, target)]), axis=0),
+        "ssim": np.mean(np.stack([SSIM(p, t) for p, t in zip(pred_numpy, target_numpy)]), axis=0),
+        "psnr": np.mean(np.stack([PSNR(p, t) for p, t in zip(pred_numpy, target_numpy)]), axis=0),
+        "mse": np.mean(np.stack([MSE(p, t) for p, t in zip(pred_numpy, target_numpy)]), axis=0),
+        "lpips": torch.mean(torch.stack([LPIPS(p, t) for p, t in zip(pred_torch, target_torch)]), axis=0).item(),
     }
 
 if __name__ == '__main__':
 
-    a, b = torch.rand(8, 5, 3, 93, 124), torch.rand(8, 5, 3, 93, 124)  # [b, t, c, h, w]
+    a, b = torch.rand(8, 50, 3, 93, 124), torch.rand(8, 50, 3, 93, 124)  # [b, t, c, h, w]
     a, b = 2*a-1, 2*b-1
 
-    for metric, val in get_image_metrics(a, a*0.95).items():
+    for metric, val in get_image_metrics(a, b).items():
         print(f"{metric}: {val}")
