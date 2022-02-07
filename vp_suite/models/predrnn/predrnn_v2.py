817175a2d5c430af97412b7307ee1453076d0cab
Andreas Boltres <boltres@ais.uni-bonn.de>
2022-02-07 14:11:38 +0100

    added action-conditioning to PredRNN++

--- vp_suite/models/predrnn/predrnn_v2.py
+++ vp_suite/models/predrnn/predrnn_v2.py
@@ -2,7 +2,7 @@ import math
 import torch
 import torch.nn as nn
 from vp_suite.models.model_blocks.predrnn import SpatioTemporalLSTMCell as STCell,\
-    ActionConditionalSpatioTemporalLSTMCell as ActionConditionalSTCell
+    ActionConditionalSpatioTemporalLSTMCell as ACSTCell
 from vp_suite.base.base_model import VideoPredictionModel
 import torch.nn.functional as F
 from tqdm import tqdm
@@ -10,7 +10,17 @@ from tqdm import tqdm
 
 class PredRNN_V2(VideoPredictionModel):
     r"""
-    Non-action ONLY
+    This is a reimplementation of the model "PredRNN-V2", as introduced in "PredRNN: A Recurrent Neural Network for
+    Spatiotemporal Predictive Learning" by Wang et al. (https://arxiv.org/pdf/2103.09504.pdf). This implementation
+    is based on the official PyTorch implementation on https://github.com/thuml/predrnn-pytorch.
+
+    PredRNN-V aims at learning partly disentangled spatial/temporal dynamics of the input domain and use this to
+    render more accurate predicted frames. The "Spatio-Temporal LSTM Cell" forms the heart of this model.
+
+    Note:
+        This model will use the whole frame sequence as an input, including the frames to be predicted. If you do not
+        have a ground truth prediction for your frame sequence, pad the sequence with t "zero" frames, with t being
+        the amount of predicted frames.
     """
 
     # model-specific constants
@@ -38,6 +48,8 @@ class PredRNN_V2(VideoPredictionModel):
     r_exp_alpha: int = 5000
     training_iteration: int = None
     sampling_eta: float = None
+    conv_actions_on_input: bool = True
+    residual_on_action_conv: bool = True
 
     def pred_1(self, x, **kwargs):
         return self(x, pred_length=1, **kwargs)
@@ -45,43 +57,96 @@ class PredRNN_V2(VideoPredictionModel):
     def __init__(self, device, **model_kwargs):
         super(PredRNN_V2, self).__init__(device, **model_kwargs)
 
-        self.NON_CONFIG_VARS.extend(["training_iteration, sampling_eta"])
         self.patch_c = self.patch_size * self.patch_size * self.img_c  # frame channel
-        self.patch_h = self.img_h // self.patch_size  # cell height
-        self.patch_w = self.img_w // self.patch_size  # cell width
+        self.patch_h = self.rnn_h = self.img_h // self.patch_size  # cell/RNN height
+        self.patch_w = self.rnn_w = self.img_w // self.patch_size  # cell/RNN width
+        self.conv_actions_on_input = self.conv_actions_on_input and self.action_conditional  # set to false if not A.C.
+        self.residual_on_action_conv = self.residual_on_action_conv and self.conv_actions_on_input  # N/A if no a.-conv.
 
+        # in action-conditional mode, setting conv_actions_on_input to True results in a slightly different comp. graph
+        if self.conv_actions_on_input:
+            self.rnn_h /= 4
+            self.rnn_w /= 4
+            self.conv_input1 = nn.Conv2d(self.patch_c, self.num_hidden[0] // 2, self.filter_size,
+                                         stride=2, padding=self.filter_size // 2, bias=False)
+            self.conv_input2 = nn.Conv2d(self.num_hidden[0] // 2, self.num_hidden[0], self.filter_size,
+                                         stride=2, padding=self.filter_size // 2, bias=False)
+            self.action_conv_input1 = nn.Conv2d(self.action_size, self.num_hidden[0] // 2, self.filter_size,
+                                                stride=2, padding=self.filter_size // 2, bias=False)
+            self.action_conv_input2 = nn.Conv2d(self.num_hidden[0] // 2, self.num_hidden[0], self.filter_size,
+                                                stride=2, padding=self.filter_size // 2, bias=False)
+            self.deconv_output1 = nn.ConvTranspose2d(self.num_hidden[self.num_layers - 1],
+                                                     self.num_hidden[self.num_layers - 1] // 2,
+                                                     self.filter_size, stride=2, padding=self.filter_size // 2,
+                                                     bias=False)
+            self.deconv_output2 = nn.ConvTranspose2d(self.num_hidden[self.num_layers - 1] // 2, self.patch_c,
+                                                     self.filter_size, stride=2, padding=self.filter_size // 2,
+                                                     bias=False)
+
+        # assemble cell list
         cell_list = []
-
+        cell_class = ACSTCell if self.action_conditional else STCell
         for i in range(self.num_layers):
-            in_channel = self.patch_c if i == 0 else self.num_hidden[i - 1]
+            if i == 0:
+                if self.conv_actions_on_input:
+                    in_channel = self.patch_c + self.action_size
+                elif self.action_conditional:
+                    in_channel = self.num_hidden[0]
+                else:
+                    in_channel = self.patch_c
+            else:
+                in_channel = self.num_hidden[i - 1]
             cell_list.append(
-                STCell(in_channel, self.num_hidden[i], self.patch_h, self.patch_w,
-                       self.filter_size, self.stride, self.layer_norm)
+                cell_class(in_channel, self.num_hidden[i], self.rnn_h, self.rnn_w,
+                           self.filter_size, self.stride, self.layer_norm)
             )
         self.cell_list = nn.ModuleList(cell_list)
-        self.conv_last = nn.Conv2d(self.num_hidden[self.num_layers - 1], self.patch_c,
-                                   kernel_size=1, stride=1, padding=0, bias=False)
-        # shared adapter
-        adapter_num_hidden = self.num_hidden[0]
-        self.adapter = nn.Conv2d(adapter_num_hidden, adapter_num_hidden, 1, stride=1, padding=0, bias=False)
+
+        # conv_last (non-existent for when conv_actions_on_input is True)
+        if self.action_conditional and not self.conv_actions_on_input:
+            self.conv_last = nn.Conv2d(self.num_hidden[self.num_layers - 1], self.patch_c + self.action_size,
+                                       kernel_size=1, stride=1, padding=0, bias=False)
+        elif not self.action_conditional:
+            self.conv_last = nn.Conv2d(self.num_hidden[self.num_layers - 1], self.patch_c,
+                                       kernel_size=1, stride=1, padding=0, bias=False)
+
+        # adapter
+        adap_num_hidden = self.num_hidden[self.num_layers - 1] if self.action_conditional else self.num_hidden[0]
+        self.adapter = nn.Conv2d(adap_num_hidden, adap_num_hidden, kernel_size=1, stride=1, padding=0, bias=False)
+
+        # training variables
         self.training_iteration = 1
         self.sampling_eta = 1.0
+        self.NON_CONFIG_VARS.extend(["training_iteration, sampling_eta"])
 
     def forward(self, x, pred_frames: int = 1, **kwargs):
-        b, total_frames = x.shape[:2]  # NOTE: x NEEDS TO HAVE 'TOTAL_FRAMES' FRAMES!
+
+        # prep variables
+        b, total_frames, _, img_h, img_w = x.shape  # NOTE: x NEEDS TO HAVE 'TOTAL_FRAMES' FRAMES!
         context_frames = total_frames - pred_frames
         train = kwargs.get("train", False)
-        mask_true = self._scheduled_sampling(b, context_frames, pred_frames, train)
-        x_patch = self._reshape_patch(x)  # [b, t, cpp, h_, w_]
 
+        # prep inputs
+        empty_actions = torch.zeros(b, total_frames, device=self.device)
+        if self.action_conditional:
+            actions = kwargs.get("actions", empty_actions)
+            if actions.equal(empty_actions) or actions.shape[-1] != self.action_size:
+                raise ValueError("Given actions are None or of the wrong size!")
+            actions = actions[..., None, None].expand(-1, -1, -1, img_h, img_w)  # [b, t, a, h, w]
+            xa = torch.cat([x, actions], dim=2)  # [b, t, c+a, h, w]
+            xa_patch = self._reshape_patch(xa)  # [b, t, cpp + app, h_, w_]
+            x_patch, a_patch = torch.split(xa_patch, self.patch_c, dim=2)
+        else:
+            x_patch = self._reshape_patch(x)  # [b, t, cpp, h_, w_]
+            a_patch = None
+
+        # cell iteration preparation
         next_frames = []
         h_t = []
         c_t = []
         delta_c_list = []
         delta_m_list = []
-
         decouple_loss = []
-
         for i in range(self.num_layers):
             zeros = torch.zeros([b, self.num_hidden[i], self.patch_h, self.patch_w]).to(self.device)
             h_t.append(zeros)
@@ -90,55 +155,91 @@ class PredRNN_V2(VideoPredictionModel):
             delta_m_list.append(zeros)
 
         memory = torch.zeros([b, self.num_hidden[0], self.patch_h, self.patch_w]).to(self.device)
+        mask_true = self._scheduled_sampling(b, context_frames, pred_frames, train)
         x_gen = None
 
+        # actual fwd pass
         for t in range(total_frames - 1):
             if self.reverse_scheduled_sampling:
                 # reverse schedule sampling
                 if t == 0:
                     net = x_patch[:, t]
                 else:
-                    net = mask_true[:, t - 1] * x_patch[:, t] + (1 - mask_true[:, t - 1]) * x_gen
+                    net = mask_true[:, t - 1] * x_patch[:, t] \
+                          + (1 - mask_true[:, t - 1]) * x_gen
             else:
                 # schedule sampling
                 if t < context_frames:
                     net = x_patch[:, t]
                 else:
-                    net = mask_true[:, t - context_frames] * x_patch[:, t] + (1 - mask_true[:, t - context_frames]) * x_gen
+                    net = mask_true[:, t - context_frames] * x_patch[:, t] \
+                          + (1 - mask_true[:, t - context_frames]) * x_gen
 
-            h_t[0], c_t[0], memory, delta_c, delta_m = self.cell_list[0](net, h_t[0], c_t[0], memory)
+            if self.conv_actions_on_input:
+                net_shape1 = net.shape
+                net = self.conv_input1(net)
+                if self.residual_on_action_conv:
+                    input_net1 = net
+                net_shape2 = net.shape
+                net = self.conv_input2(net)
+                if self.residual_on_action_conv:
+                    input_net2 = net
+                a_patch = self.action_conv_input1(a_patch)
+                a_patch = self.action_conv_input2(a_patch)
+
+            if self.action_conditional:
+                h_t[0], c_t[0], memory, delta_c, delta_m = self.cell_list[0](net, h_t[0], c_t[0], memory, a_patch)
+            else:
+                h_t[0], c_t[0], memory, delta_c, delta_m = self.cell_list[0](net, h_t[0], c_t[0], memory)
             delta_c_list[0] = F.normalize(self.adapter(delta_c).view(delta_c.shape[0], delta_c.shape[1], -1), dim=2)
             delta_m_list[0] = F.normalize(self.adapter(delta_m).view(delta_m.shape[0], delta_m.shape[1], -1), dim=2)
 
             for i in range(1, self.num_layers):
-                h_t[i], c_t[i], memory, delta_c, delta_m = self.cell_list[i](h_t[i - 1], h_t[i], c_t[i], memory)
+                if self.action_conditional:
+                    h_t[i], c_t[i], memory, delta_c, delta_m = self.cell_list[0](h_t[i - 1], h_t[i], c_t[i], memory, a_patch)
+                else:
+                    h_t[i], c_t[i], memory, delta_c, delta_m = self.cell_list[0](h_t[i - 1], h_t[i], c_t[i], memory)
                 delta_c_list[i] = F.normalize(self.adapter(delta_c).view(delta_c.shape[0], delta_c.shape[1], -1), dim=2)
                 delta_m_list[i] = F.normalize(self.adapter(delta_m).view(delta_m.shape[0], delta_m.shape[1], -1), dim=2)
 
-            x_gen = self.conv_last(h_t[self.num_layers - 1])
-            next_frames.append(x_gen)
             # decoupling loss
             for i in range(0, self.num_layers):
                 decouple_loss.append(
                     torch.mean(torch.abs(torch.cosine_similarity(delta_c_list[i], delta_m_list[i], dim=2))))
-        decouple_loss = torch.mean(torch.stack(decouple_loss, dim=0))
 
+            if self.conv_actions_on_input:
+                if self.residual_on_action_conv:
+                    x_gen = self.deconv_output1(h_t[self.num_layers - 1] + input_net2, output_size=net_shape2)
+                    x_gen = self.deconv_output2(x_gen + input_net1, output_size=net_shape1)
+                else:
+                    x_gen = self.deconv_output1(h_t[self.num_layers - 1], output_size=net_shape2)
+                    x_gen = self.deconv_output2(x_gen, output_size=net_shape1)  # [b, cpp, h_, w_]
+            elif self.action_conditional:  # NOT EQUIVALENT TO ORIGINAL: discard predicted actions
+                x_gen = self.conv_last(h_t[self.num_layers - 1])[:, :self.patch_c]  # [b, cpp, h_, w_]
+            else:
+                x_gen = self.conv_last(h_t[self.num_layers - 1])  # [b, cpp, h_, w_]
+            next_frames.append(x_gen)
+
+        # finalize
         predictions_patch = torch.stack(next_frames, dim=1)  # [b, t, c, h, w]
         predictions = self._reshape_patch_back(predictions_patch)
+        decouple_loss = torch.mean(torch.stack(decouple_loss, dim=0))
         return predictions, {"ST-LSTM decouple loss": decouple_loss}
 
     def _reshape_patch(self, x):
         b, t, c, h, w = x.shape
-        if self.img_shape != (c, h, w):
-            raise ValueError(f"shape mismatch: expected {self.img_shape}, got {(c, h, w)}")
+        expected_img_c = self.img_c + self.action_size if self.action_conditional else self.img_c
+        expected_input_shape = (expected_img_c, self.img_h, self.img_w)
+        if expected_input_shape != (c, h, w):
+            raise ValueError(f"shape mismatch: expected {expected_input_shape}, got {(c, h, w)}")
         a = torch.reshape(x, [b, t, c, self.patch_h, self.patch_size, self.patch_w, self.patch_size])
         b = torch.permute(a, (0, 1, 2, 4, 6, 3, 5))  # [b, t, c, p, p, h_, w_]
-        x_patch = torch.reshape(b, [b, t, self.patch_c, self.patch_h, self.patch_w])
+        x_patch = torch.reshape(b, [b, t, -1, self.patch_h, self.patch_w])  # infer channel dim automatically since there might be actions included
         return x_patch
 
     def _reshape_patch_back(self, x_patch):
-        b, t = x_patch.shape[:2]
-        c = self.patch_c // (self.patch_size * self.patch_size)
+        b, t, cpp = x_patch.shape[:3]
+        c = cpp // (self.patch_size * self.patch_size)
         h = self.patch_h * self.patch_size
         w = self.patch_w * self.patch_size
         a = torch.reshape(x_patch, [b, t, c, self.patch_size, self.patch_size, self.patch_h, self.patch_w])
