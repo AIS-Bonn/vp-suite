6cb1b29ed03f5d066ea48356644ebc9a1ab86bf7
Andreas Boltres <boltres@ais.uni-bonn.de>
2022-03-17 12:38:05 +0100

    Minor fix in about file

--- vp_suite/__about__.py
+++ vp_suite/__about__.py
@@ -14,6 +14,7 @@ __docs_url__ = "https://ais-bonn.github.io/vp-suite/"
 __source_url__ = "https://github.com/AIS-Bonn/vp-suite"
 __tracker_url__ = "https://github.com/AIS-Bonn/vp-suite/issues"
 __docs__ = ( "A Framework for Training and Evaluating Video Prediction Models" )
+__python_requires__ = ">=3.6"
 __keywords__ = ["deep learning", "pytorch", "AI", "video prediction"]
 __classifiers__ = [
         "Environment :: Console",
@@ -22,8 +23,7 @@ __classifiers__ = [
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
-    ],
-__python_requires__ = ">=3.6"
+    ]
 
 __all__ = ["__name__", "__version__", "__author__", "__author_email__", "__license__",
            "__copyright__", "__homepage__", "__docs_url__", "__source_url__",
